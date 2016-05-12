e20e9c9b8dffc6531472904d9f1277a159ac468b
sichenwang <52870104@qq.com>
2016-05-12 21:42:58 +0800

    修改获取图片资源的方法

--- SCPictureBrowser/SCPictureBrowser/SCActivityIndicatorView.m
+++ SCPictureBrowser/SCPictureBrowser/SCActivityIndicatorView.m
@@ -7,6 +7,7 @@
 //
 
 #import "SCActivityIndicatorView.h"
+#import "UIImage+Bundle.h"
 
 @interface UIView(Extension)
 
@@ -75,20 +76,20 @@
         case SCActivityIndicatorViewStyleDefault:
             self.layer.cornerRadius = 5;
             self.backgroundColor = [UIColor colorWithRed:241/255.0 green:241/255.0 blue:241/255.0 alpha:1];
-            self.imageView.image = [UIImage imageNamed:@"loading_circle"];
+            self.imageView.image = [UIImage bundleImageNamed:@"loading_circle"];
             self.size = CGSizeMake(44, 44);
             self.imageView.center = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
             break;
         case SCActivityIndicatorViewStyleCircle:
             self.layer.cornerRadius = 0;
             self.backgroundColor = [UIColor clearColor];
-            self.imageView.image = [UIImage imageNamed:@"loading_circle"];
+            self.imageView.image = [UIImage bundleImageNamed:@"loading_circle"];
             self.size = self.imageView.size;
             break;
         case SCActivityIndicatorViewStyleCircleLarge:
             self.layer.cornerRadius = 0;
             self.backgroundColor = [UIColor clearColor];
-            self.imageView.image = [UIImage imageNamed:@"loading_circle_large"];
+            self.imageView.image = [UIImage bundleImageNamed:@"loading_circle_large"];
             self.size = self.imageView.size;
             break;
     }
